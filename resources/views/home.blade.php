821c02a42f5059ba5a3a544ea6b624bb1161c343
MAAZAZ <zakitche@gmail.com>
2020-06-26 04:14:48 +0100

    up

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <h3>hello</h3>
+@endsection
---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+    <h1>{{ $d['titre'] }}</h1>
+    {!! $d2 !!}
+@endsection
